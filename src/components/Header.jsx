74e58ae5abdbd2c99c536f345647bdf2a30136d8
Rudra-Maity <maityrudra228@gmail.com>
2025-05-24 21:23:41 +0530

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Bell, Plus, Search } from "lucide-react";
 export default function Header({setSidebarOpen,sidebarOpen}) {
     return (
          <header className="bg-white shadow-sm border-b">
-          <div className="flex items-center justify-between px-6 py-4">
-            <div className="flex items-center gap-4">
+          <div className="flex items-center justify-between px-6 py-4 flex-wrap gap-4">
+            <div className="flex items-center gap-4 ">
               <button 
                 onClick={() => setSidebarOpen(!sidebarOpen)}
                 className="lg:hidden p-2 rounded-lg hover:bg-gray-100"
@@ -24,12 +24,13 @@ export default function Header({setSidebarOpen,sidebarOpen}) {
                   className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64"
                 />
               </div>
-              <div className="flex items-center gap-4">
+              <div className="flex items-center gap-4 flex-wrap">
               <Bell className="w-6 h-6 text-gray-600 cursor-pointer hover:text-gray-800" />
               
             </div>
             </div>
-            <div className='flex gap-4' >
+            
+            <div className='flex gap-4 flex-wrap ' >
               <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center">
               
                 <span className="text-white text-sm font-semibold">A</span>
